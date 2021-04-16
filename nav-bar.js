0a57ee412f5d23ae24af5e180d0ecc63e0116be0
Manjiri Vadujkar <manjiri.vadujkar@gmail.com>
2021-04-16 17:49:55 +0530

    style changes

--- nav-bar.js
+++ nav-bar.js
@@ -1,23 +1,25 @@
 // <head><link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"></head>
-// this statements needed in the body
-// <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-// 				<script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-// 				<script src="./nav-bar.js?id=index" id="nav-bar"></script>
+// foll statements needed in the body
+//<script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+//<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+//<script src="./nav-bar.js?id=index" id="nav-bar"></script> 				
+// 				
 
 $(function () {
     var bar = '';
-    bar += '<nav class="navbar navbar-inverse" role="navigation">';
+    bar += '<nav class="navbar navbar-expand-lg bg-dark navbar-dark mynav" role="navigation">';
     bar += '<div class="container-fluid">';
     bar += '<div>';
-    bar += '<ul class="nav navbar-nav">';
-    bar += '<li id="home"><a href="home.html">Home</a></li>';
-    bar += '<li id="index"><a href="index.html">Index</a></li>';
-    bar += '<li id="about"><a href="about.html">About</a></li>';
+    bar += '<ul class="navbar-nav">';
+    bar += '<li class="nav-item" id="home"><a class="nav-link" href="home.html">Home</a></li>';
+    bar += '<li class="nav-item" id="about"><a class="nav-link" href="about.html">About</a></li>';
+    bar += '<li class="nav-item" id="gallery"><a class="nav-link" href="gallery.html">Gallery</a></li>';
+    bar += '<li class="nav-item" id="timeline"><a class="nav-link" href="timeline.html">Timeline</a></li>';
     bar += '</ul>';
     bar += '</div>';
     bar += '</div>';
     bar += '</nav>';
-		bar += '<style>.navbar-inverse .navbar-nav>.active>a, .navbar-inverse .navbar-nav>.active>a:focus, .navbar-inverse .navbar-nav>.active>a:hover {background-color:#9b3675;}nav.navbar-inverse ul.navbar-nav a:hover,nav.navbar-inverse ul.navbar-nav a:visited,nav.navbar-inverse ul.navbar-nav a:focus,nav.navbar-inverse ul.navbar-nav a:active{background:#9b3675;}</style>'
+		bar += '<style>.container-fluid {align-items: initial !important;}.nav-item {margin-left: 5px; margin-right: 5px} .mynav {font-size: 28px;}.mynav a {color: rgba(255,255,255,1) !important;}.navbar-dark .navbar-nav>.active>a, .navbar-dark .navbar-nav>.active>a:focus, .navbar-dark .navbar-nav>.active>a:hover {background-color:#702085;}nav.navbar-dark ul.navbar-nav a:hover,nav.navbar-dark ul.navbar-nav a:visited,nav.navbar-dark ul.navbar-nav a:focus,nav.navbar-dark ul.navbar-nav a:active{background:#702085;}</style>'
     $("#main-bar").html(bar);
  
     var id = getValueByName("id");
